8d3596c5aebf65c32a9768d668964fcecefaa7dd
Joseph Lyons <jclyons52@gmail.com>
2016-07-25 19:31:18 +1000

    ifxed action visibility for posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
     <div class="container">
+        @if(Auth::check() && Auth::user()->isAdmin())
         <div class="row">
             <h1>Posts</h1>
             <div class="pull-right">
                 <a href="/posts/create" class="btn btn-primary">Add New</a>
             </div>
         </div>
+        @endif
     <table class="table">
         <thead>
         <th>Title</th>
-        <th>Content</th>
         <th>Posted by</th>
         <th>Created at</th>
         <th>Actions</th>
@@ -20,11 +21,11 @@
         @foreach($posts as $post)
             <tr>
                 <td>{{$post->title}}</td>
-                <td>{!! $post->content !!}</td>
                 <td>{{$post->user->name}}</td>
                 <td>{{$post->created_at}}</td>
                 <td>
                     <a href="/posts/{{$post->id}}" class="btn btn-primary">view</a>
+                    @if(Auth::check() && Auth::user()->isAdmin())
                     <a href="/posts/{{$post->id}}/edit" class="btn btn-success">edit</a>
                     <form action="/posts/{{$post->id}}" method="post" style="display: inline;">
                         {{csrf_field()}}
@@ -33,7 +34,7 @@
                             delete
                         </button>
                     </form>
-
+@endif
                 </td>
             </tr>
         @endforeach
