36e68a912b0efb9a59556461144eb35a91d1a626
Administrator <jclyons52@gmail.com>
2016-09-05 12:29:17 +1000

    added missing api route deleted routes file when upgrading to laravel 5.3, this route got lost as it was not merged at the time

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,6 @@ Route::group(['namespace' => 'API\V1', 'prefix' => '/api/v1'], function () {
     Route::get('/jobs/watching', 'JobsController@watching');
 
     Route::get('/jobs/owned', 'JobsController@owned');
+
+    Route::get('/jobs/search', 'JobsController@search');
 });
