671db1e0ff7ef8de4e10e1a79d800433b5963360
Administrator <jclyons52@gmail.com>
2016-09-05 17:20:57 +1000

    added trusted proxy package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Fideloper\Proxy\TrustProxies::class,
     ];
 
     /**
